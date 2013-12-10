b43cceb602fbca63304852374015b10a8134deed
Nitin Borwankar <nborwankar@gmail.com>
2013-12-10 11:37:14 -0500

    added a number of shell scripts that are known to work on a bare linux box - they have been used to bootstrap this environment on a Digital Ocean droplet.  The run-learnds-vagrant.sh script needs to be tested

---
+++ bsd2/docker/run-learnds-linux.sh
@@ -0,0 +1,1 @@
+docker run -p 127.0.0.1:8888:8888 -v /LearnDataScience:/LearnDataScience -w /LearnDataScience/notebooks nitin/learn_data_science
---
+++ bsd2/docker/run-learnds-vagrant.sh
@@ -0,0 +1,1 @@
+docker run -p 127.0.0.1:8888:8888 -v /vagrant/docker/content/LearnDataScience:/LearnDataScience -w /LearnDataScience/notebooks nitin/learn_data_science
