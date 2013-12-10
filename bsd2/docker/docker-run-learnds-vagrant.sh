0bdd4bd3901e56f83852ffd1595a98278fc2bb71
Dav Clark <davclark@berkeley.edu>
2013-12-10 11:54:49 -0800

    Fixed vagrant script

--- bsd2/docker/docker-run-learnds-vagrant.sh
+++ bsd2/docker/docker-run-learnds-vagrant.sh
@@ -1,1 +1,1 @@
-docker run -p 127.0.0.1:8888:8888 -v /vagrant/docker/content/LearnDataScience:/LearnDataScience -w /LearnDataScience/notebooks nitin/learn_data_science
+docker run -p 127.0.0.1:8888:8888 -v /vagrant/LearnDataScience:/LearnDataScience -w /LearnDataScience/notebooks nitin/learn_data_science
